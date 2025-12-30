5a37667870fdd35caebc00216f4aaa14ce36d7cd
Light <furqatshanazarov9@gmail.com>
2025-12-30 16:46:22 +0500

    Initial Django project upload

---
+++ main/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import RegisterForm, ItemForm
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    items = Item.objects.all().order_by('-date')
+    return render(request, 'home.html', {'items': items})
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def add_item(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.author = request.user
+            item.save()
+            return redirect('/')
+    else:
+        form = ItemForm()
+
+    return render(request, 'add_item.html', {'form': form})
